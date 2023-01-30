51c2531981be807f3518a7c9f7fd739ebfdc9c7d
Kim-Seong-Ho <kksshh0612@gmail.com>
2023-01-30 17:19:52 +0900

    컴포넌트 스캔까지 공부 완료

--- core-principle/src/main/java/seongho/coreprinciple/member/MemoryMemberRepository.java
+++ core-principle/src/main/java/seongho/coreprinciple/member/MemoryMemberRepository.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class MemoryMemberRepository implements MemberRepository{        //MemberRepository 인터페이스의 구현 클래스
+public class MemoryMemberRepository implements MemberRepository{        //MemberRepository 인터페이스의 구현 클래스임
 
     private static Map<Long, Member> store = new HashMap<>();           //Long과 Member형 객체를 값으로 하는 Map
 
