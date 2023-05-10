42cc4dd976cc900f4c96bbca9de62597f47638d7
Kim-Seong-Ho <kksshh0612@gmail.com>
2023-05-10 15:02:40 +0900

    [JPA-basic] 연관관계 매핑 다대다 수정

--- jpa-basic/src/main/java/helloJpa/Member.java
+++ jpa-basic/src/main/java/helloJpa/Member.java
@@ -1,7 +1,12 @@
 package helloJpa;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 //@SequenceGenerator(
@@ -12,6 +17,7 @@ public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+    @Column(name = "MEMBER_ID")
     private Long id;
 
     @Column(name = "name")              //객체는 username, 디비는 name으로 쓰고싶을 때
@@ -25,23 +31,8 @@ public class Member {
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
 
-
-//    @Column(name = "TEAM_ID")     //테이블에 맞춘 설계
-//    private Long teamId;
-
-//    private Integer age;                //그냥 쓰면 디비에 Integer랑 적절한 숫자 타입이 만들어짐
-//
-//    @Enumerated(EnumType.STRING)        //Enum타입을 쓸 수 있다.
-//    private RoleType roleType;
-//
-//    @Temporal(TemporalType.TIMESTAMP)   //날짜 타입을 만들 수 있다. Date, Time, Timestamp 이렇게 세개가 enum으로 있음
-//    private Date createdDate;
-//
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date lastModifiedDate;
-//
-//    @Lob                                //큰 컨텐츠를 넣을 때
-//    private String description;
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Member(){}       //JPA를 사용할 때, 생성자를 만들면, 이렇게 기본 생성자를 꼭 만들어줘야 한다.
 
@@ -56,14 +47,6 @@ public class Member {
     public void setUsername(String username) {
         this.username = username;
     }
-
-//    public Long getTeamId() {
-//        return teamId;
-//    }
-//
-//    public void setTeamId(Long teamId) {
-//        this.teamId = teamId;
-//    }
 
     public Team getTeam() {
         return team;
--- jpa-basic/src/main/java/helloJpa/Team.java
+++ jpa-basic/src/main/java/helloJpa/Team.java
@@ -1,6 +1,8 @@
 package helloJpa;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
