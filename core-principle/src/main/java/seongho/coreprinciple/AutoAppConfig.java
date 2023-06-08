b5767328f6f78e86d0f939ea3572e16681d826e6
Kim-Seong-Ho <kksshh0612@gmail.com>
2023-06-08 14:00:39 +0900

    [core-principle] 복습

--- core-principle/src/main/java/seongho/coreprinciple/AutoAppConfig.java
+++ core-principle/src/main/java/seongho/coreprinciple/AutoAppConfig.java
@@ -7,7 +7,8 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
 //        basePackages = "seongho.coreprinciple",
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)     //AppConfig를 제외함.
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)     //AppConfig를 제외함. 기존 코드 버리지 않기 위해서 한거임.
+        //원래는 설정정보도 컴포넌트 스캔 대상
 )
 public class AutoAppConfig {
 
--- core-principle/src/main/java/seongho/coreprinciple/CorePrincipleApplication.java
+++ core-principle/src/main/java/seongho/coreprinciple/CorePrincipleApplication.java
@@ -3,7 +3,7 @@ package seongho.coreprinciple;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication	//이 안에 @ComponentScan 있음
 public class CorePrincipleApplication {
 
 	public static void main(String[] args) {
